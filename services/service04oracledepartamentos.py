a0dc29ab01c383fdd596d8f3a4b5190f4fe39870
serraguti <pacoserranox@gmail.com>
2025-02-27 16:43:05 +0100

    upodate service

--- services/service04oracledepartamentos.py
+++ services/service04oracledepartamentos.py
@@ -6,6 +6,15 @@ class ServiceOracleDepartamentos:
         #CREAMOS UN OBJETO CONNECTION
         self.connection = oracledb.connect(user='SYSTEM', password='oracle', dsn='localhost/xe')
     
+    def modificarDepartamento(self, numero, nombre, localidad):
+        sql = "update DEPT set DNOMBRE=:p1, LOC=:p2 where DEPT_NO=:p3"
+        cursor = self.connection.cursor()
+        cursor.execute(sql, (nombre, localidad, numero))
+        registros = cursor.rowcount
+        self.connection.commit()
+        cursor.close()
+        return registros
+
     def eliminarDepartamento(self, numero):
         sql = "delete from DEPT where DEPT_NO=:p1"
         cursor = self.connection.cursor()
